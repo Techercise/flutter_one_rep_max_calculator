68869c07b7618167f3d9c257b89c13dfa560987d
techercise <mjl1997@verizon.net>
2023-11-16 13:29:26 -0500

    Updating files for Android MVP release

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 
+
 import 'package:flutter/material.dart';
 import 'package:flutter_one_rep_max_calculator/calculator.dart' as calc;
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
 
@@ -10,11 +12,11 @@ void main() {
       appBar: AppBar(
         backgroundColor: const Color(0xff00c853),
         title: const Text('One Rep Max Calculator'),
-        leading: IconButton(
-          icon: const Icon(Icons.menu),
-          tooltip: 'Menu Button',
-          onPressed: () {},
-        ),
+        // leading: const IconButton(
+        //   icon: Icon(Icons.menu),
+        //   tooltip: 'Menu Button',
+        //   onPressed: () {},
+        // ),
       ),
       body: const calc.Calculator(),
       bottomNavigationBar: const BottomAppBar(
@@ -25,6 +27,40 @@ void main() {
           overflowAlignment: OverflowBarAlignment.center,
         ),
       ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const [
+            SizedBox(
+              height: 40.0,
+              child: DrawerHeader(
+                padding: EdgeInsets.zero,
+                margin: EdgeInsets.zero,
+                child: Text('Settings Menu'),
+              ),
+            ),
+            ListTile(
+              title: Text('View Privacy Policy'),
+              onTap: _launchURL,
+            )
+          ],
+        )
+      ),
     ),
   ));
+}
+
+// void menuButtonPressed()
+// {
+//   Widget build(BuildContext context)
+//   {
+//     return
+//   }
+// }
+
+_launchURL() async {
+  final Uri privacyPolicy = Uri.parse('https://sites.google.com/site/techerciseapps/orm-privacy-policy');
+  if (!await launchUrl(privacyPolicy)) {
+    throw Exception('Could not launch $privacyPolicy');
+  }
 }
